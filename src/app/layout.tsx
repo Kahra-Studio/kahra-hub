aef1e1b8f538787e9c5228d24274166c504bc1a2
Umut Türkkahraman <tkmnumut@gmail.com>
2025-12-22 07:36:10 +0300

    feat: initialize project with Next.js, Tailwind CSS, Radix UI, and core UI components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import { NavbarDefault } from "@/components/container/navbar";
+import { FooterDefault } from "@/components/container/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font = Outfit({
   subsets: ["latin"],
 });
 
@@ -24,10 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${font} max-w-6xl mx-auto antialiased py-6 space-y-12`}>
+        <NavbarDefault />
         {children}
+        <FooterDefault />
       </body>
     </html>
   );
